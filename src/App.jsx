3ea60996055afce14d8b8fa59f4d900599b1a370
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-03 10:21:27 -0300

    feat(App): adiciona componentes criados

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import About from './components/About';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
 
 function App () {
   return (
     <div>
-      <h1>Test</h1>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
   )
 }
 
-export default App
+export default App;
