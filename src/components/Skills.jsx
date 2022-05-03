c6987f8f5d970e0c7c00c409434fc240a19948f1
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-03 10:20:58 -0300

    feat(Skills): cria estrutura HTML

---
+++ src/components/Skills.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Skills extends Component {
+  render() {
+    return (
+      <div>
+        <h3>My Skills</h3>
+        <p>ausheuahseuhasheuasheuah
+          aisuheiuasheiusae
+          iasheiuasheiuahsiueiauseiu
+        </p>
+        <div>
+          <button>Temp IMAGE</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Skills;
