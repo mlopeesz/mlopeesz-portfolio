af305bbcf14b27ef85c7ef228a440abef2e0a0ad
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-03 10:21:13 -0300

    feat(Footer): cria estrutura HTML

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Mauricio Lopes</h2>
+          <p>Desenvolvedor Web Fullstack</p>
+        </div>
+        <div>
+          <h3>Links</h3>
+          <a>Home</a>
+          <a>Sobre</a>
+          <a>Skills</a>
+          <a>Projetos</a>
+          <a>Contato</a>
+        </div>
+        <div>
+          <h3>Redes Sociais</h3>
+          <a>face</a>
+          <a>insta</a>
+          <a>email</a>
+          <a>zap</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
