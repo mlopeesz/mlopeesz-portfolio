f39b8402226b355955002d48651be548af5482c1
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-03 10:20:34 -0300

    feat(Home): cria estrutura HTML

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Prazer, <span>Mauricio Lopes</span></h2>
+          <h1>Desenvolvedor Web FullStack</h1>
+        </div>
+        <p>Código simples, bonito e funcional.</p>
+        <button>Temp Image</button>
+      </div>
+    );
+  }
+}
+
+export default Home;
