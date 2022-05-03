7fcf2d31afdfce5354b11a3a9483ef00294628a4
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-03 15:20:10 -0300

    feat(css): estilização básica Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import memojiNotebook from '../assets/memojis/memoji-notebook.png';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <h2>Prazer, <span>Mauricio Lopes</span></h2>
-          <h1>Desenvolvedor Web FullStack</h1>
+      <div className="text-white flex flex-col my-6">
+        <div className="flex justify-center my-2">
+          <h2 className="text-2xl">Prazer,
+          <a href="https://www.linkedin.com/in/mlopeesz/" target="_blank" className="text-yellow-select mx-1.5 underline" rel="noreferrer">Mauricio Lopes</a>👋</h2>
         </div>
-        <p>Código simples, bonito e funcional.</p>
-        <button>Temp Image</button>
+        <div className="flex justify-center text-3xl">
+          <h1>Desenvolvedor Web Fullstack</h1>
+        </div>
+        <div className="flex justify-center my-8">
+          <p>Código simples, bonito e funcional.</p>
+        </div>
+        <div className="flex justify-center">
+          <img src={memojiNotebook} alt="memoji-notebook" className="w-60" />
+        </div>
       </div>
     );
   }
