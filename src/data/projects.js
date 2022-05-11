ad10657a200ff8f928769b89d08a4b45079c8b5b
Mauricio <mlopeesz@gmail.com>
2022-05-11 15:29:25 -0300

    feat: cria array com dados dos projetos

---
+++ src/data/projects.js
@@ -0,0 +1,14 @@
+const projects = [
+  {
+    name: 'projeto 1',
+    description: 'esse projeto asduias asid asdk asd asdk asdio asi',
+    stacks: 'HTML, CSS, GRULLI'
+  },
+  {
+    name: 'projeto 2',
+    description: 'esse projasdsid asdk asasdaasdsdio asi',
+    stacks: 'HTML, CSS, GRULLI, nelsetas'
+  }
+];
+
+export default projects;
