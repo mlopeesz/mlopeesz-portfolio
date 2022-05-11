71a9d423f6dbb79ebc86f468b5a403f93b8c6396
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-11 18:18:35 -0300

    build: instalação de dependencias

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import 'tw-elements';
 
 function App () {
   return (
