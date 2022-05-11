6b20960d4c595ee80300775966f59579b5f6d0cb
Mauricio <mlopeesz@gmail.com>
2022-05-11 16:14:22 -0300

    feat: tentativa map array

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import projects from '../data/projects';
 
 class Projects extends Component {
   render() {
@@ -6,7 +7,13 @@ class Projects extends Component {
       <div id="Projects" className="text-white w-full h-screen flex flex-col justify-center items-center flex-wrap snap-start">
         <h2 className="bg-dark-bg text-yellow-select rounded text-[28px] w-52 text-center mt-14">Projetos</h2>
         <div>
-          <h4>Novidades em breve.</h4>
+          {projects.map((project) => {
+            <div>
+              <h2>{project.name}</h2>
+              <p>{project.description}</p>
+              <p>{project.stacks}</p>
+            </div>
+          })}
         </div>
       </div>
     );
