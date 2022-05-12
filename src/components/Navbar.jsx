ce28845c21f3c03624ebcdaf2217d2bedbf3c19b
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-12 10:10:57 -0300

    feat: Navbar 2.0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,61 @@
 import React, { Component } from 'react';
-import logo from '../assets/icons/computer.svg'
 
-class Navbar extends Component {
+class NavbarTailwind extends Component {
   render() {
     return (
-      <header className="bg-dark-bg flex justify-between items-center h-16 p-4 box-border">
-        <img src={logo} alt="computer logo" className="w-8"/>
-        <nav>
-          <ul className="flex gap-2">
-            <li><a className="hover:text-yellow-select" href="#Home">Home</a></li>
-            <li><a className="hover:text-yellow-select" href="#About">Sobre</a></li>
-            <li><a className="hover:text-yellow-select" href="#Skills">Skills</a></li>
-            <li><a className="hover:text-yellow-select" href="#Projects">Projetos</a></li>
-            <li><a className="hover:text-yellow-select" href="#Footer">Contato</a></li>
-          </ul>
+      <header>
+        <nav className="navbar navbar-expand-lg shadow-md py-2 bg-dark-bg flex items-center w-full justify-between fixed">
+          <div className="px-6 w-full flex flex-wrap items-center justify-between">
+            <div className="flex items-center">
+              <button
+                className="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-white hover:text-yellow-select focus:text-yellow-select transition-shadow duration-150 ease-in-out mr-2"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContentY"
+                aria-controls="navbarSupportedContentY"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  className="w-5"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 448 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+            <div className="navbar-collapse collapse grow items-center" id="navbarSupportedContentY">
+              <ul className="navbar-nav mr-auto lg:flex lg:flex-row">
+                <li className="nav-item">
+                  <a className="nav-link block pr-2 lg:px-2 py-2 text-white hover:text-yellow-select focus:text-yellow-select transition duration-150 ease-in-out" href="#Home" data-mdb-ripple="true" data-mdb-ripple-color="light">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link block pr-2 lg:px-2 py-2 text-white hover:text-yellow-select focus:text-yellow-select transition duration-150 ease-in-out" href="#About" data-mdb-ripple="true" data-mdb-ripple-color="light">Sobre mim</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link block pr-2 lg:px-2 py-2 text-white hover:text-yellow-select focus:text-yellow-select  transition duration-150 ease-in-out" href="#Skills" data-mdb-ripple="true" data-mdb-ripple-color="light">Habilidades</a>
+                </li>
+                <li className="nav-item mb-2 lg:mb-0">
+                  <a className="nav-link block pr-2 lg:px-2 py-2 text-white hover:text-yellow-select focus:text-yellow-select transition duration-150 ease-in-out" href="#Projects" data-mdb-ripple="true" data-mdb-ripple-color="light">Projetos</a>
+                </li>
+                <li className="nav-item mb-2 lg:mb-0">
+                  <a className="nav-link block pr-2 lg:px-2 text-white hover:text-yellow-select focus:text-yellow-select transition duration-150 ease-in-out" href="#Footer" data-mdb-ripple="true" data-mdb-ripple-color="light">Contato</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </nav>
       </header>
     );
   }
 }
 
-export default Navbar;
+export default NavbarTailwind;
