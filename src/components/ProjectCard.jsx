fde56bc2a25473e762e71da749eee4c8184a90f0
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-16 18:19:51 -0300

    GITHUB

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -25,7 +25,7 @@ class ProjectCard extends Component {
                       type="button"
                       className="inline-block px-6 py-2.5 bg-gray-700 text-yellow-select font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-800 hover:shadow-lg focus:bg-gray-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out"
                     >
-                      Github
+                      GITHUB
                     </button>
                   </a>
                 </div>
