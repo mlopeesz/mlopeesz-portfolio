bab1087ac14bdbe2fc71001f410303fe1d9e0b39
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-16 18:00:44 -0300

    feat: importa card em sessão de projetos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import ProjectCarousel from './ProjectCarousel';
+import ProjectCard from './ProjectCard';
 
 class Projects extends Component {
   render() {
     return (
-      <div id="Projects" className="text-white w-full 
-       flex flex-col justify-center items-center snap-start gap-4">
-        <h2 className="bg-dark-bg text-yellow-select rounded text-[28px] w-52 text-center mt-14">Projetos</h2>
-        <ProjectCarousel />
+      <div
+        id="Projects"
+        className="text-white w-full 
+       flex flex-col justify-center items-center snap-start gap-4"
+      >
+        <h2 className="bg-dark-bg text-yellow-select rounded text-[28px] w-52 text-center mt-14">
+          Projetos
+        </h2>
+        <ProjectCard />
       </div>
     );
   }
