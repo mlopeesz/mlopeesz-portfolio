fd4183f08188038f3f36ab97c27f9e5f0ede4765
Mauricio Lopes <mlopeesz@gmail.com>
2022-05-19 19:25:51 -0300

    fix: css

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,29 +6,26 @@ class ProjectCard extends Component {
     return (
       <div className="flex flex-wrap gap-2 m-4">
         {projects.map((project) => (
-          <div key={project.name} className="flex justify-center">
-            <div className="rounded-lg shadow-lg bg-dark-bg max-w-sm">
+          <div
+            key={project.name}
+            className="flex rounded-lg shadow-lg bg-dark-bg max-w-sm"
+          >
+            <div>
               <a href={project.github}>
                 <img className="rounded-t-lg" src={project.img} alt="" />
               </a>
-              <div className="p-6">
-                <h5 className="text-gray-100 text-xl font-medium mb-2">
-                  {project.name}
-                </h5>
-                <p className="text-gray-200 text-base mb-4">
-                  {project.description}
-                </p>
-                <h6>{project.stacks}</h6>
-                <div className="flex flex-wrap gap-2">
-                  <a href={project.github} target="_blank" rel="noreferrer">
-                    <button
-                      type="button"
-                      className="inline-block px-6 py-2.5 bg-gray-700 text-yellow-select font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-800 hover:shadow-lg focus:bg-gray-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out"
-                    >
-                      GITHUB
-                    </button>
-                  </a>
-                </div>
+              <div className="flex flex-col justify-between text-gray-200 gap-4 m-4">
+                <h5 className="text-xl font-medium">{project.name}</h5>
+                <p>{project.description}</p>
+                <span>{project.stacks}</span>
+                <a href={project.github} target="_blank" rel="noreferrer">
+                  <button
+                    type="button"
+                    className="px-6 py-2.5 bg-gray-700 text-yellow-select font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-800 hover:shadow-lg focus:bg-gray-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150"
+                  >
+                    GITHUB
+                  </button>
+                </a>
               </div>
             </div>
           </div>
