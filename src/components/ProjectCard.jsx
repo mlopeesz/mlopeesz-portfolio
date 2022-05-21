650869d5b399c6b9d05250a2d621d83c610215a2
Mauricio <mlopeesz@gmail.com>
2022-05-21 12:40:02 -0300

    fix: padroniza cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,25 +8,25 @@ class ProjectCard extends Component {
         {projects.map((project) => (
           <div
             key={project.name}
-            className="flex rounded-lg shadow-lg bg-dark-bg max-w-sm"
+            className="rounded-lg shadow-lg bg-dark-bg max-w-sm"
           >
             <div>
               <a href={project.github}>
                 <img className="rounded-t-lg" src={project.img} alt="" />
               </a>
-              <div className="flex flex-col justify-between text-gray-200 gap-4 m-4">
-                <h5 className="text-xl font-medium">{project.name}</h5>
+              <div className="flex flex-col gap-4 text-gray-200 m-4 h-[320px]">
+                <h5 className="text-xl font-bold">{project.name}</h5>
                 <p>{project.description}</p>
-                <span>{project.stacks}</span>
+                <span className="font-semibold mt-auto">{project.stacks}</span>
+              </div>
                 <a href={project.github} target="_blank" rel="noreferrer">
                   <button
                     type="button"
-                    className="px-6 py-2.5 bg-gray-700 text-yellow-select font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-800 hover:shadow-lg focus:bg-gray-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150"
+                    className="w-full px-6 py-2.5 bg-gray-700 text-yellow-select font-medium text-xs rounded shadow-md hover:bg-gray-800 hover:shadow-lg focus:bg-gray-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150"
                   >
                     GITHUB
                   </button>
                 </a>
-              </div>
             </div>
           </div>
         ))}
